357379e954205d43079fdd1243f5798af351193f
xie.gang <xiegang2@maccura.com>
2023-09-20 11:26:59 +0800

    c++17: use std::invoke_result_t instead of std::result_of.

--- ThreadPool.h
+++ ThreadPool.h
@@ -63,12 +63,22 @@ template<class F, class... Args>
 auto ThreadPool::enqueue(F&& f, Args&&... args) 
     -> std::future<typename std::result_of<F(Args...)>::type>
 {
+#if __cplusplus >= 201703L
+    // in virtual studio, the __cplusplus may be not correct;
+    // add "/Zc:__cplusplus" in comand line text to fix the problem;
+    // please reference: https://learn.microsoft.com/en-us/cpp/build/reference/zc-cplusplus?view=msvc-170
+
+    // c++17 use std::invoke_result_t instead of std::result_of.
+    using return_type = typename std::invoke_result_t<F, Args...>;
+#else
+    // c++11~c++14
     using return_type = typename std::result_of<F(Args...)>::type;
+#endif
 
     auto task = std::make_shared< std::packaged_task<return_type()> >(
             std::bind(std::forward<F>(f), std::forward<Args>(args)...)
         );
-        
+
     std::future<return_type> res = task->get_future();
     {
         std::unique_lock<std::mutex> lock(queue_mutex);
